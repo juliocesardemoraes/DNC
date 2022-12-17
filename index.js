1e931c098b58e24aaff34cbeb0ef015a6e3d92e2
julio cesar <jcmcf2012@gmail.com>
2022-12-17 12:33:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const form = document.getElementById("formAddMed");
+
+//PEGAR TODOS OS INPUTS QUE FOREM DESTE FORMULARIO DE ADICIONAR
+const allInputs = infoForm.getElementsByTagName("input");
+const showModal = document.getElementById("modal");
+
+/*
+for (let i = 0; i < allInputs.length; i++) {
+  allInputs[i].addEventListener("invalid", function (event) {
+    if (event.target.validity.valueMissing) {
+      const showModal = document.getElementById("modal");
+      showModal.classList.remove("hide");
+    }
+  });
+}
+*/
+
+const addMed = () => {
+  event.preventDefault();
+
+  const med = {
+    medName: document.getElementById("medName").value,
+    medQuantity: document.getElementById("medQuantity").value,
+    medPrice: document.getElementById("medPrice").value,
+    medClass: document.getElementById("medClass").value,
+  };
+
+  //SUMIR COM O FORM
+  form.classList.add("hide");
+
+  //APARECER COM AS INFORMAÇÕES
+  const formInfo = document.getElementById("formInfo");
+  formInfo.classList.remove("hide");
+  //INSERIR ESSAS INFORMAÇÔES
+  const medInfoName = document.getElementById("medInfoName");
+  const medInfoQuantity = document.getElementById("medInfoQuantity");
+  const medInfoPrice = document.getElementById("medInfoPrice");
+
+  medInfoName.innerHTML = med.medName;
+  medInfoQuantity.innerHTML = `${med.medQuantity} Unidades disponíveis`;
+  medInfoPrice.innerHTML = `${med.medPrice} Reais por unidade`;
+};
+
+//APARECER COM AS INFORMAÇÕES
+
+const returnForm = () => {
+  //SUMIR COM AS INFORMAÇÕES
+  const formInfo = document.getElementById("formInfo");
+  formInfo.classList.add("hide");
+  //APARECER COM O FORM
+  const form = document.getElementById("formAddMed");
+  form.classList.remove("hide");
+};
