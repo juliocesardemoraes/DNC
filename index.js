a546ecdb4c97e92c357d83129dab80f0743e4598
julio cesar <jcmcf2012@gmail.com>
2023-09-15 14:40:35 -0300

    feat: add day 08 from dnc

---
+++ index.js
@@ -0,0 +1,15 @@
+const signupForm = document.getElementById("form");
+
+signupForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+
+const signUp = () => {
+  const inputName = document.getElementsByClassName("input__name")[0];
+  const inputEmail = document.getElementsByClassName("input__email")[0];
+  if (inputName.value === "" || inputEmail.value === "") {
+    alert("Não foi possível efetivar seu cadastro.");
+  } else {
+    alert("Cadastro efetuado com sucesso");
+  }
+};
