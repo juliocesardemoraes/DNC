1f58c6a5538baff5066a6320644350dc78804cac
julio cesar <jcmcf2012@gmail.com>
2023-09-28 10:17:43 -0300

    infra: first setup for this project

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+var setaesq = window.document.getElementById("setaesq")
+var leo = window.document.getElementById("leo")
+var sama = window.document.getElementById("sama")
+var brun = window.document.getElementById("brun")
+var setadir = window.document.getElementById("setadir")
+
+function roldir () {
+
+leo.style = "display:none"
+brun.style = "display:flex" 
+sama.style = "display:none"
+setadir.style = "display:flex"
+setaesq.style = "display:none"
+
+}
+
+
+
+function rolesq () {
+    leo.style = "display:none"
+    brun.style = "display:flex"
+    sama.style = "display:none" 
+    setadir.style = "display:none;"
+    setaesq.style = "display:flex"
+    
+    }
